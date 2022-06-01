c61cffdef6a1de21c8b94970040b5db08d976185
Hugo Voisin <hugov@bam.tech>
2022-06-01 21:40:53 +0200

    feat: add custom template for specific google user

--- src/pages/Home/openPreFilledEmail.ts
+++ src/pages/Home/openPreFilledEmail.ts
@@ -1,3 +1,4 @@
+import { getGoogleUserEmail } from 'api/sheets/getGoogleUserEmail'
 import { openEmail } from 'libs/openEmail'
 
 export const openPreFilledEmail = (
@@ -8,6 +9,11 @@ export const openPreFilledEmail = (
   downloadLink: string
 ) => {
   const emailSubject = `Ton passage au ${fiealdEdition}e Fieald !`
-  const emailBody = `Bonjour ${artistName},\n\nLa vidéo de ton passage au ${fiealdEdition}e Fieald du ${fiealdEditionDate} est téléchargeable sur ce lien : ${downloadLink}\n\nElle sera disponible une semaine sur le lien, mais nous te conseillons de la sauvegarder sur un support personnel.\n\nEn te souhaitant une bonne réception,\nL'équipe vidéo\n`
+  let emailBody = `Bonjour ${artistName},\n\nLa vidéo de ton passage au ${fiealdEdition}e Fieald du ${fiealdEditionDate} est téléchargeable sur ce lien : ${downloadLink}\n\nElle sera disponible une semaine sur le lien, mais nous te conseillons de la sauvegarder sur un support personnel.\n\nEn te souhaitant une bonne réception,\nL'équipe vidéo\n`
+
+  const userEmail = getGoogleUserEmail()
+  if (userEmail === 'hpvoisin@gmail.com') {
+    emailBody = `Bonjour ${artistName},\n\nLa vidéo de ton passage au ${fiealdEdition}e Fieald du ${fiealdEditionDate} est téléchargeable sur ce lien : ${downloadLink}\n\nElle sera disponible une semaine sur le lien, mais nous te conseillons de la sauvegarder sur un support personnel.\n\nEn te souhaitant une bonne réception,\nHugo de l'équipe vidéo\n`
+  }
   openEmail(artistEmail, emailSubject, emailBody)
 }
