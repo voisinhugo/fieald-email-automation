7e30488e63711d90a94bc049796ab3fb1c0c94dc
Hugo Voisin <hugov@bam.tech>
2023-01-13 22:00:09 +0100

    feat: add fieald edition date to email subject as the edition number is not enough for the artists

--- src/pages/Home/openPreFilledEmail.ts
+++ src/pages/Home/openPreFilledEmail.ts
@@ -7,7 +7,7 @@ export const openPreFilledEmail = (
   artistEmail: string,
   downloadLink: string
 ) => {
-  const emailSubject = `Ton passage au ${fiealdEdition}e Fieald !`
+  const emailSubject = `Ton passage au ${fiealdEdition}e Fieald du ${fiealdEditionDate} !`
   const emailBody = `Bonjour ${artistName},\n\nLa vidéo de ton passage au ${fiealdEdition}e Fieald du ${fiealdEditionDate} est téléchargeable sur ce lien : ${downloadLink}\n\nElle y sera disponible une semaine, mais nous te conseillons de la sauvegarder sur un support personnel.\n\nEn te souhaitant une bonne réception,\nL'équipe vidéo\n`
 
   openEmail(artistEmail, emailSubject, emailBody)
